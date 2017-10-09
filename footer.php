aaac88c18825113a3ebf9b57475c861ff3f34d65
Norris <im@justnorris.com>
2017-10-09 15:40:23 +0300

    Minimal footer

--- footer.php
+++ footer.php
@@ -1,18 +1,10 @@
 
 <footer id="footer" class="site-footer" role="contentinfo">
 	<div class="site-info">
-
-		<span class="sitemeta">
-				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'unstyled' ) ); ?>"><?php printf(
-						esc_html__( 'Proudly powered by %s.', 'unstyled' ),
-						'WordPress'
-					); ?></a>
-		</span>
-
 		<span class="sitemeta">
 				<?php printf(
-					esc_html__( 'Theme: %1$s.', 'unstyled' ),
-					'<a href="https://github.com/justnorris/unstyled">Unstyled</a>'
+					esc_html__( 'Arctic Avocado theme by %1$s.', 'unstyled' ),
+					'<a href="https://themetally.com">Themetally</a>'
 				); ?>
 		</span>
 	</div><!-- .site-info -->
