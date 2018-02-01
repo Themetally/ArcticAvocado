9abd9df0c86c98dd6c4dab46fef7e3181538b2ac
Norris <im@justnorris.com>
2018-02-01 11:18:12 +0200

    Add gridlover-stylus build task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,1 +1,41 @@
-import './assets/gulp/autoinit'
+import gulp        from 'gulp'
+import sequence    from 'run-sequence'
+import { modules } from './assets/gulp/gulp'
+import stylus      from './assets/griddle-gulp/gridlover-stylus'
+
+const {
+		  configure,
+		  yaml,
+		  browser_sync,
+		  scripts,
+		  pot,
+		  pack,
+		  version,
+		  utilities
+	  } = modules
+
+global.config = configure( yaml() )
+
+// Register Tasks
+gulp.task( 'sync', browser_sync )
+gulp.task( 'scripts', scripts )
+gulp.task( 'pot', pot )
+gulp.task( 'styl', stylus )
+gulp.task( 'pack', pack )
+gulp.task( 'version', version )
+
+gulp.task( 'build', () => {
+	gulp.start( 'styl' )
+	gulp.start( 'scripts' )
+	gulp.start( 'pot' )
+} )
+
+gulp.task( 'dist', ( cb ) => {
+	global.config.production = true
+
+	return sequence(
+		[ 'version', 'styl', 'scripts', 'pot' ],
+		'pack',
+		cb
+	)
+} )
